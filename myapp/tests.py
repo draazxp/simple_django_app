e2020e1b823a32c97839c76eb4492793e78e6503
draazxp <rduwal1804@gmail.com>
2024-06-08 02:49:16 +0545

    changed color

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import MyModel
 
-# Create your tests here.
+class MyModelTestCase(TestCase):
+    def setUp(self):
+        # Create test data or perform setup tasks
+        self.my_model = MyModel.objects.create(name='Test')
+
+    def test_model_creation(self):
+        # Retrieve the object from the database
+        obj = MyModel.objects.get(name='Test')
+        
+        # Verify that the object was created correctly
+        self.assertEqual(obj.name, 'Test')
+
