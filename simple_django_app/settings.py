bf776f78dc1b7f0af8af1776804cc99aa5a01dac
draazxp <46063877+draazxp@users.noreply.github.com>
2024-06-08 01:39:37 +0545

    Update settings.py

--- simple_django_app/settings.py
+++ simple_django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a0_2)#jcb!*otf76n5$4vqg8wwud+7f1hnc8vaiqv)+4!ug&b+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
